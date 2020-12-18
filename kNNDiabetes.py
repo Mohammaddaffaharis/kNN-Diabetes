22d34b6520e25cd908ea7204040cecfe0ba81df3
Mohammaddaffaharis <mohammaddaffaharis@gmail.com>
2020-12-18 09:35:58 +0700

    Lasstttthhh

--- kNNDiabetes.py
+++ kNNDiabetes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from math import sqrt
 
+#melakukan read data Diabetes.csv dan memmasukkan kedalam array user
 def readData():
     file = preProcess(pd.read_csv('Diabetes.csv'))
     user =[]
@@ -17,6 +18,7 @@ def readData():
         col.append(file['Outcome'][i])
         user.append(col)
     return user
+#melakukan preprocessing data (mengubah data yang bernilai nol dengan mean kolom)
 def preProcess(file):
     meanPregnancies = file['Pregnancies'].mean(skipna=True)
     file['Pregnancies'] = file.Pregnancies.mask(file.Pregnancies == 0, meanPregnancies)
@@ -35,6 +37,7 @@ def preProcess(file):
     meanAge = file['Age'].mean(skipna=True)
     file['Age'] = file.Age.mask(file.Age == 0, meanAge)
     return file
+#memecah data menjadi 2 bagian yaitu data training(80%) dan data testing(20%)
 def splitTrainTest(data):
     # Dataset 1
     training = []
@@ -83,11 +86,13 @@ def splitTrainTest(data):
         testing.append(data[i])
     data5 = training, testing
     return data1, data2, data3, data4, data5
+#fungsi yang digunakan untuk menghitung jarak antara dua baris data
 def euclidian(baris1, baris2):
     jarak = 0
     for i in range(len(baris1) - 1):
         jarak = jarak + ((baris1[i] - baris2[i]) ** 2)
     return sqrt(jarak)
+#pencarian k data training terdekat dari suatu baris data test
 def getNeighbors(train, testRow, neighborsNum):
     distance = []
     for i in train:
@@ -98,6 +103,7 @@ def getNeighbors(train, testRow, neighborsNum):
     for j in range(neighborsNum):
         neighbors.append(distance[j][0])
     return neighbors
+#prediksi nilai outcome baru dari sebuah data test
 def getClass(neighbors):
     kelas1 = 0
     kelas0 = 0
@@ -110,30 +116,39 @@ def getClass(neighbors):
         return 0
     elif (kelas0 < kelas1):
         return 1
-def getAccuracy(real, predicted):
+#menghitung nilai akurasi antara nilai outcome asli dan nilai outcome prediksi
+def getAccuracyDataset(real, predicted):
     true = 0
     for i in range(len(real)):
         if real[i] == predicted[i]:
             true = true + 1
     return (true/float(len(real))) *100
+#Main function untuk proses kNN
+def getNewOutput(dataSet, k, i):
+    oldOutput = []
+    newOutput = []
+    for j in range(len(dataSet[i][1])):
+        nearestNeighbour = getNeighbors(dataSet[i][0], dataSet[i][1][j], k)
+        newOutput.append(getClass(nearestNeighbour))
+        oldOutput.append(dataSet[i][1][j][8])
+    return oldOutput, newOutput
+#Main function untuk proses Five Fold Validation
+def fiveFoldValidation(dataSet,k):
+    for i in range(len(dataSet)):
+        score = []
+        newOutput = getNewOutput(dataSet, k, i)
+        accuracy = getAccuracyDataset(newOutput[0], newOutput[1])
+        # print("DataSet ke-", i ," dengan K= ", k , " memiliki akurasi sebesar " , accuracy)
+        score.append(accuracy)
+    return score
+#Ultimately Main Function
 def main():
     data = readData()
     dataSet = splitTrainTest(data)
-    #best 31 = 75.97402597402598
-    newk = [31git]
+    #best K 31 = 75.97402597402598
+    newk = [31]
     for k in newk:
-        accurate = 0.0
-        for i in range(len(dataSet)):
-            oldOutput = []
-            newOutput = []
-            score = []
-            for j in  range(len(dataSet[i][1])):
-                nearestNeighbour = getNeighbors(dataSet[i][0],dataSet[i][1][j],k)
-                newOutput.append(getClass(nearestNeighbour))
-                oldOutput.append(dataSet[i][1][j][8])
-            accuracy = getAccuracy(oldOutput,newOutput)
-            print("DataSet ke-", i ," dengan K= ", k , " memiliki akurasi sebesar " , accuracy)
-            score.append(accuracy)
-        print("Dengan Rata-Rata acuracy sebesar ", sum(score)/len(score))
-
+        score = fiveFoldValidation(dataSet, k)
+        avg = sum(score)/len(score)
+        print("Data dengan K =",k, "memiliki Rata-Rata acuracy sebesar", avg)
 main()
